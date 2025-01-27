4ddcc3a7fda03952ed32668829da362745f806ee
TheComputerM <mudit.somani00@gmail.com>
2025-01-28 00:24:40 +0530

    build: add script to download ffmpeg static builds

---
+++ scripts/download-ffmpeg.ts
@@ -0,0 +1,29 @@
+import { dirname, join } from "node:path";
+
+/**
+ * Collection of good ffmpeg static binaries
+ */
+const releases =
+	"https://github.com/descriptinc/ffmpeg-ffprobe-static/releases/download/b6.1.2-rc.1";
+
+/**
+ * Mapping of binaries from release to where we are storing locally
+ */
+const binaries = [["linux-x64", "x86_64-unknown-linux-gnu"]];
+
+/**
+ * Location of the sidecar dir for tauri
+ */
+const sidecarDirectory = join(
+	dirname(import.meta.dirname),
+	"./src-tauri/binaries",
+);
+
+for (const [from, to] of binaries) {
+	const target = join(sidecarDirectory, `ffmpeg-${to}`);
+	if (await Bun.file(target).exists()) continue;
+
+	const file = await fetch(`${releases}/${from}`);
+	await Bun.write(target, file);
+	console.info(`Downloaded ${from} to ${target}`);
+}
