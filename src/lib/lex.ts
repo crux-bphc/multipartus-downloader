a0aefb68558242205fd99b791aa92fc2f54d1e91
TheComputerM <mudit.somani00@gmail.com>
2025-02-16 11:44:53 +0530

    fix: refresh token when fetching from lex

--- src/lib/lex.ts
+++ src/lib/lex.ts
@@ -1,6 +1,7 @@
 import { logtoClient } from "./logto";
 
 export async function fetchLex<T>(url: string, init?: RequestInit): Promise<T> {
+	await logtoClient.getAccessToken();
 	return fetch(`https://lex.crux-bphc.com/api/impartus/${url}`, {
 		...init,
 		headers: {
