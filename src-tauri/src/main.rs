1b7146cf19f36062aee2a741b4f26d4450a5b6b6
Adithyadev R <45178461+poisonpwn@users.noreply.github.com>
2025-03-19 00:14:28 +0530

    fix: import the lib in main.rs to remove all the unused warnings

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod commands;
+use multipartus_downloader_lib::run;
 
 fn main() {
     // Load up dotenv
     dotenv::dotenv().ok();
-    multipartus_downloader_lib::run()
+    run()
 }
